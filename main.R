837f94cdc35154e315d2d96a7acb020e97a0ee73
kaksat <damian0gwozdz@gmail.com>
2017-01-07 18:56:45 +0100

    V 0.0.5
    1. Removed basic R plot.
    2. Changes in the header.
    3. Changes in the header of a function "pred.mRSE".
    4. Added calculation of RMSE to the function "pred.RMSE".
    5. Added some comments.

--- main.R
+++ main.R
@@ -2,12 +2,11 @@
 ###   TP: recruitment exercise
 #
 #
-# Author:     DG
-# Date:       29DEC2016
-# Mod. date:  -
-# Input data: data.csv
+# Author:           DG
+# Date:             29DEC2016
+# Last  mod. date:  07JAN2017
+# Input data:       data.csv
 ############################################
-
 
 ##### 0. Reading packages, setting working directory----
 
@@ -15,7 +14,8 @@ set.seed(1234567)
 
 rm(list = ls())
 
-# install.packages("nortest")
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(data.table, dplyr, ggplot2, caret, nortest, corrplot)
 
 library(data.table)
 library(dplyr)
@@ -36,6 +36,9 @@ tail(data.videos)
 dim(data.videos)
 sum(is.na(data.videos))
 qplot(apply(data.videos[,2:ncol(data.videos)], 1, mean), geom = "histogram")
+
+# Naming variables properly
+
 names(data.videos) <- c("id", paste0("V", 1:168))
 
 ##### (1) Distribution of V(168), basic stats for v({24, 72, 168})----
@@ -57,6 +60,10 @@ ad.test(log(data.videos[, "V168"]))
 shapiro.test(log(data.videos[, "V168"]))
 mean(log(data.videos[, "V168"]))
 
+# The distribution looks a bit similar to the normal one, however both Shapiro
+# and Anderson Darling test suggest rejection of null hypothesis of the data 
+# being distributed normally.
+
 # (1.4) Outliers of v(168)
 
 data.videos$ln.V168 <- log(data.videos$V168)
@@ -65,21 +72,28 @@ sd.ln.V168 <- sd(data.videos$ln.V168)
 low.3sigma <- mean.ln.V168-sd.ln.V168*3
 high.3sigma <- mean.ln.V168+sd.ln.V168*3
 
+# Removing outliers
+
 data.videos <- data.videos[data.videos$ln.V168>=low.3sigma &
                              data.videos$ln.V168<=high.3sigma,]
-
-dim(data.videos)
 
 # (1.5) Correlation between log-transformed v(1:24) and log-transformed v(168)
 
 for(i in 1:24){
+  
+  # Adding log-transformed V1-V24
+  
   data.videos[[paste0("ln.V", i)]] <- log(data.videos[[paste0("V", i)]])
   
-  # If calculating a natural logarithm results in Inf, then log(1) is inputted
+  # If calculating a natural logarithm results in Inf, mean from this variable
+  # is inputted
   
   data.videos[[paste0("ln.V", i)]] <- ifelse(data.videos[[paste0("ln.V", i)]] == -Inf,
-                                             log(1), data.videos[[paste0("ln.V", i)]])
+                                             mean(data.videos$ln.V1[data.videos$ln.V1 != -Inf]),
+                                             data.videos[[paste0("ln.V", i)]])
 }
+
+mean(data.videos$ln.V1[data.videos$ln.V1 != -Inf])
 
 log.cor <- cor(data.videos[, c(paste0("ln.V", c(1:24, 168)))])
 
@@ -92,39 +106,22 @@ split.vec <- sample(1:nrow(data.videos), size = floor(0.9*nrow(data.videos)))
 data.train <- data.videos[split.vec,]
 data.test <- data.videos[-split.vec,]
 
-# (1.7) OLS for v(168)
-
-# 1.7.1 Adding logarithms for the rest of variables
-
-for(i in 25:167){
-  data.videos[[paste0("ln.V", i)]] <- log(data.videos[[paste0("V", i)]])
-  
-  # If calculating a natural logarithm results in Inf, then log(1) is inputted
-  
-  data.videos[[paste0("ln.V", i)]] <- ifelse(data.videos[[paste0("ln.V", i)]] == -Inf,
-                                             log(1), data.videos[[paste0("ln.V", i)]])
-}
-
-# (1.9) Evaluation of predictors
+# (1.7-9) OLS, multiple-input OLS & evaluation of predictors
 
 pred.mRSE <- function(vars, target, train, test){
   
-#   vars <- "ln.V1"
-#   target <- "ln.V168"
-#   train <- data.train
-#   test <- data.test
-#   i <- 1
-  # rm(vars, target, train, test, i)
-  
-  ####################################################################
+  #####################################################################
   # (I) INFO:
   #
   # Author:     DG
   # Date:       04JAN2017
   # Mod. date:  
   # Purpose:    To evaluate Oridnary Least Squares by mean Relative
-  #             Squared Error (mRSE) on the test dataset.
+  #             Squared Error (mRSE) on the test dataset and
+  #             Root Mean Square Error (RMSE) on training data set
   # Input dataset(s): train (training data), test (test data)
+  # Output(s):        list containing 2 vectors:
+  #                   $mRSE.test; $RMSE.train
   #
   # 
   # (II)  PARAMETERS:
@@ -132,10 +129,12 @@ pred.mRSE <- function(vars, target, train, test){
   # 1.  train     - training data
   # 2.  test      - test data
   # 3.  target    - target variable
-  # 4.  var       - 
-  ####################################################################
+  # 4.  var       - vector of variables. If multiple predictors are
+  #                 specyfied, they should be named in one string
+  #                 with a blank as a separator, e.g. "Var1 Var2 Var3".
+  #####################################################################
   
-  mRSE <- c()
+  mRSE.test <- RMSE.train <- c()
   model <- pred.vec <- list()
   
   for(i in 1:length(vars)){
@@ -146,19 +145,42 @@ pred.mRSE <- function(vars, target, train, test){
     
     model[[i]] <- lm(formula.lm, data = train)
     
+    # RMSE on a training set
+    
+    RMSE.train <- c(RMSE.train,
+                    sum((model[[i]]$fitted - train[[target]])^2)/nrow(train))
+    
     # Prediction on a test set
     
     pred.vec[[i]] <- predict(model[[i]], test)
     
     # mRSE calculation
     
-    mRSE <- c(mRSE, sum((pred.vec[[i]]/test[[target]] - 1)^2) / nrow(test))
+    mRSE.test <- c(mRSE.test, sum((pred.vec[[i]]/test[[target]] - 1)^2) / nrow(test))
   }
-  return(mRSE)
+  return(list(mRSE.test = mRSE.test, RMSE.train = RMSE.train))
 }
 
+# Creating a vector with variable names
+
 pred.single.var <- paste0("ln.V", 1:24)
+
+# Prediction for models with single regresors
+
 mRSE.single.var <- pred.mRSE(pred.single.var, "ln.V168", data.train, data.test)
+mRSE.single.var$mRSE.test
+mRSE.single.var$RMSE.train
+# Checking which model resulted in minimum mRSE
+
+which(mRSE.single.var$mRSE.test == min(mRSE.single.var$mRSE.test)) # == 24,
+# therefore the model which
+# minimizes mRSE is the one with log-transformed V24
+
+which(mRSE.single.var$RMSE.train == min(mRSE.single.var$RMSE.train)) # == also 24.
+# therefore model minimizing RMSE on training data set is the one with 
+# log-transformed V24
+
+# Prediction for models with multiple regresors
 
 pred.multiple.vars <- c()
 for(i in 1:24){
@@ -167,7 +189,19 @@ for(i in 1:24){
 }
 mRSE.multiple.vars <- pred.mRSE(pred.multiple.vars, "ln.V168", data.train, data.test)
 
-# Preparing data for ggplot2
+# Checking which model resulted in minimum mRSE
+
+which(mRSE.multiple.vars$mRSE.test == min(mRSE.multiple.vars$mRSE.test)) # == 24,
+# therefore the model which
+# minimizes mRSE is the one with all log-transformed 24 variables
+
+which(mRSE.multiple.vars$RMSE.train == min(mRSE.multiple.vars$RMSE.train)) # == also 24.
+# therefore model minimizing RMSE on training data set is the one with 
+# all log-transformed 24 variables
+
+# (1.10) mRSE visualization----
+
+# Preparing data for ggplot2 - melting in order to plot multiple lines on one plot
 
 mRSE.compare <- cbind(index = seq_along(mRSE.single.var), mRSE.single.var, 
                       mRSE.multiple.vars) %>%
@@ -180,29 +214,17 @@ mRSE.compare[,2] <- ifelse(mRSE.compare[,2] == "mRSE.single.var",
                            "Linear Regression",
                            "Multiple-input Linear Regression")
 
-
-# (1.10) Plot the mRSE
-
-plot(mRSE.single.var, type = "l", xlab = "Reference time (n)", ylab = "mRSE",
-     main = "Performance of linear regression models for n in <1: 24> hours
-      measured as mean Relative Squared Error (mRSE)", lty = 2)
-grid()
-lines(mRSE.multiple.vars, type = "l", col = "green")
-legend("topright", "(x,y)",
-       legend = list("Linear Regression", "Multiple-input Linear Regression"), 
-       lty = c(2, 1),
-       col = c("black", "green"),
-       bg = "white")
-
-# Alternative plot in ggplot2
+# Plot in ggplot2
 
 ggplot(mRSE.compare, aes(x = index, y = value, colour = variable)) + 
-      geom_line(size = 1.2) +
-      geom_point() +
-      xlab("Reference time (n)") +
-      ylab("mRSE") +
-      ggtitle("Performance of linear regression models for n in <1: 24> hours
+  geom_line(size = 1.2) +
+  geom_point() +
+  xlab("Reference time (n)") +
+  ylab("mRSE") +
+  scale_x_continuous(breaks = seq(0, 25, 3)) +
+  scale_y_continuous(breaks = seq(0, 0.003, 0.0005)) +
+  ggtitle("Performance of linear regression models for n in <1: 24> hours
   measured as mean Relative Squared Error (mRSE)") +
-      theme_minimal() +
+  theme_minimal() +
+  theme(legend.position = "bottom", legend.title=element_blank()) +
   labs(fill="Serologic response")
-
